4c3003f67d67f714da0cfcfbab5c325803a3d224
coderman <coderman@mail.com>
2025-05-13 06:56:11 +0300

    work

--- all/all/settings.py
+++ all/all/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,7 +140,6 @@ REST_FRAMEWORK = {
 }
 
 # Настройки Simple JWT
-from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
--- all/material/views.py
+++ all/material/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+
+
+def material_list(request):
+    return JsonResponse({'message': 'Materials API works!'})
